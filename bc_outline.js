93d03aa4d0005051d36a6fa5690244ec06eed5a3
JRSeabaugh1s <JRSeabaugh1s@capetigers.com>
2023-03-13 09:57:38 -0500

    first commit

---
+++ bc_outline.js
@@ -0,0 +1,56 @@
+"use strict";
+
+/*
+   New Perspectives on HTML5, CSS3 and JavaScript 6th Edition
+   Tutorial 12
+   Tutorial Case
+
+   Author: Jaedan
+   Date:   3/13/23  
+
+   Filename: bc_outline.js
+
+
+   Function List
+   =============
+
+   makeOutline()
+      Generates the text of the table of contents
+      as a nested list
+
+   createList(source, TOCList, headings)
+      Creates an outline based on the source document,
+      list items are appended to TOCList,
+      the list items are based on the element names
+      specified in the headings array
+
+
+*/
+
+// Generate an outline on h1 through h6 headings in the source document
+
+window.addEventListener("load", makeOutline);
+
+function makeOutline() {
+   // Location of the document outline
+   var outline = document.getElementById("outline");
+
+   //Source document for the outline
+   var source = document.getElementById("doc");
+   
+   var mainHeading = document.createElement("h1");
+   var outlineList = document.createElement("ol");
+   var headingText = document.createTextNode("Outline");
+
+   mainHeading.appendChild(headingText);
+   outline.appendChild(mainHeading);
+   outline.appendChild(outlineList);
+}
+
+function createList(source, outline) {
+   // Loop through all of the child nodes of source article until no child nodes are left
+
+   for (var n = source.firstChild; n !== null; n = n.nextSibling) {
+      
+   }
+}
